7db16987285b256756b8e9546db5df6ed912d961
aalsberge <aalsberge@homeaway.com>
2022-09-14 18:59:05 -0500

    moved to its own repo

---
+++ components/header.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+
+import logout from "../api/admin-user/logout"
+
+export default class extends Component {
+    requestLogout = () => {
+        logout(function() {
+            window.location.href = "/login"
+        })
+    }
+
+    render () {
+        return (
+            <div className="header-wrapper">
+                <div className="header-logo">
+                    <a href="/">
+                        <span>Admin Dashboard</span>
+                    </a>
+                </div>
+                <div className="header-log-out">
+                    <div onClick={() => this.requestLogout()} className="header-log-out">
+                        <span>Logout</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
